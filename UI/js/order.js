3fcb24dee1dccd768cf91f1efe687f19a74f0c48
Roghashin <rtimbiti@gmail.com>
2018-12-04 01:23:23 +0300

    Add price calculator

--- UI/js/order.js
+++ UI/js/order.js
@@ -7,7 +7,12 @@ window.onload = () => {
   dlg_wrapper.style.display = "none"
   dlg_box.style.display = "none"
 
+  dlg_body = document.getElementById("dlg-body")
+  price_span = document.getElementsByTagName("span")[0]
+
   order_btn.onclick = () => {
+      const price = Math.floor(Math.random() * 24) + 8
+      price_span.innerHTML = price
       dlg_wrapper.style.display = "block"
       dlg_box.style.display = "block"
   }
