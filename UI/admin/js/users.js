5551c672660c38d83d65a71615d77af4cb60140f
Roghashin <rtimbiti@gmail.com>
2018-12-10 13:01:00 +0300

    Modify selectors

--- UI/admin/js/users.js
+++ UI/admin/js/users.js
@@ -1,50 +1,53 @@
 window.onload = () => {
-  dlg_wrapper = document.getElementsByClassName("dlg-wrapper")[0]
-  dlg_box = document.getElementsByClassName("dlg-box")[0]
-  dlg_trash = document.getElementsByClassName("trash")
-  dlg_footer = document.getElementsByClassName("dlg-footer")
-  status_btn = document.getElementsByClassName("status")[0].firstElementChild
-  dropdown = document.getElementsByClassName("dropdown")[0]
+    dlg_wrapper = document.getElementsByClassName("dlg-wrapper")[0]
+    dlg_box = document.getElementsByClassName("dlg-box")[0]
+    dlg_trash = document.getElementsByClassName("trash")
+    dlg_footer = document.getElementsByClassName("dlg-footer")
+    status_btn = document.getElementsByClassName("status")[0].firstElementChild
+    dropdown = document.getElementsByClassName("dropdown")[0]
 
-  all = dropdown.getElementsByClassName("all")[0]
-  active = dropdown.getElementsByClassName("active")[0]
-  not_active = dropdown.getElementsByClassName("not-active")[0]
+    all = dropdown.getElementsByClassName("all")[0]
+    active = dropdown.getElementsByClassName("active")[0]
+    not_active = dropdown.getElementsByClassName("not-active")[0]
 
-  list_item = document.getElementsByClassName("list-item")
-  item_not_active = document.getElementsByClassName("item-not-active")
-  item_active = document.getElementsByClassName("item-active")    
-    
-  dlg_wrapper.style.display = "none"
-  dlg_box.style.display = "none"
+    list_item = document.getElementsByClassName("list-item")
+    item_not_active = document.getElementsByClassName("item-not-active")
+    item_active = document.getElementsByClassName("item-active")
 
-  for(i=0; i<dlg_trash.length; i++){
-      dlg_trash[i].onclick = () => {
-          dlg_wrapper.style.display = "block"
-          dlg_box.style.display = "block"
-      }
-  }
+    dlg_wrapper.style.display = "none"
+    dlg_box.style.display = "none"
 
-  for(i=0; i<dlg_footer.length; i++){
-      dlg_footer[i].firstElementChild.onclick = () => {
-          dlg_wrapper.style.display = "none"
-          dlg_box.style.display = "none"
-      }
-  }
+    for (i = 0; i < dlg_trash.length; i++) {
+        dlg_trash[i].onclick = () => {
+            dlg_wrapper.style.display = "block"
+            dlg_box.style.display = "block"
+        }
+    }
 
-  status_btn.onclick = () => {
-      dropdown.classList.toggle("show_dropdown")
-  }
+    for (i = 0; i < dlg_footer.length; i++) {
+        dlg_footer[i].firstElementChild.onclick = () => {
+            dlg_wrapper.style.display = "none"
+            dlg_box.style.display = "none"
+        }
+    }
 
-  all.onclick = () => {
-      
-  }
-  
-  active.onclick = () => {
-      
-  }
+    status_btn.onclick = () => {
+        dropdown.classList.toggle("show_dropdown")
+    }
 
-  not_active.onclick = () => {
-      
-  }
+    all.onclick = () => {
+        item_active.style.display = "block"
+        item_not_active.style.display = "block"
+    }
+
+    active.onclick = () => {
+        item_active.style.display = "block"
+        item_not_active.style.display = "none"
+    }
+
+    not_active.onclick = () => {
+        item_active.style.display = "block"
+        item_not_active.style.display = "none"
+    }
 
 }
